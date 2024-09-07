320c14ab1d82e070a1c1a9c14533ff38e3af6563
shreykiee <shreyash33345@gmail.com>
2024-09-07 11:56:49 +0530

    New changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,3 @@ void main() {
     ),
   );
 }
-// import 'dart:convert';
-
-// import 'package:expense_tracker/pages/expense.dart';
-// import 'package:flutter/material.dart';
-// import 'package:json_theme_plus/json_theme_plus.dart';
-// import 'package:flutter/services.dart'; // For rootBundle import 'dart:convert'; // For jsonDecode
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
-//   final themeJson = jsonDecode(themeStr);
-//   final theme = ThemeDecoder.decodeThemeData(themeJson)!;
-//   runApp(MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     theme: theme,
-//     home: const Expenses(),
-//     routes: {},
-//   ));
-// }
